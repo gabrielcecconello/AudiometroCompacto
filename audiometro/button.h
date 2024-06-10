fa6c7c02968720a81e17fc1fd243b03d85878d2a
paulatalim <paulactalim@gmail.com>
2024-06-10 19:28:12 -0300

    uniao de codigos

---
+++ audiometro/button.h
@@ -0,0 +1,91 @@
+#define PIN_BUTTON_GREEN 5
+#define PIN_BUTTON_WHITE 19
+
+void task_button_green(void *pvParameters) {
+  int button_state = 0;
+  bool is_button_press = false;
+  pinMode(PIN_BUTTON_GREEN, INPUT);
+  
+  for(;;) {
+    button_state = digitalRead(PIN_BUTTON_GREEN);
+    if(button_state == HIGH && !is_button_press) {
+      is_button_press = true;
+
+      if(display_interface == 0) {
+        if(display_opcao_selecionada == 1) {
+          display_interface = 2;
+          display_opcao_selecionada = 0;
+          xTaskCreate(taskPlayFrequencies, "Play Frequencies", 2048, NULL, 2, &handlePlayFrequencies);
+        
+        } else if(display_opcao_selecionada == 2) {
+          display_interface = 1;
+          display_opcao_selecionada = 0;
+          // TODO Inicializar player
+        }
+      
+      } else if(display_interface == 1) {
+        if(display_opcao_selecionada < display_limite_opcao) {
+          display_opcao_selecionada ++;
+        } else {
+          display_interface = 0;
+          display_opcao_selecionada = 0;
+        }
+      
+      } else if(display_interface == 2) {
+        if(display_opcao_selecionada < display_limite_opcao) {
+          display_opcao_selecionada ++;
+          isButtonPressed = true;
+        } else {
+          display_interface = 3;
+        }
+      
+      } else if (display_interface == 3) {
+        display_interface = 0;
+        display_opcao_selecionada = 0;
+      }
+
+    } else if(button_state == LOW && is_button_press) {
+      is_button_press = false;
+    }
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
+
+void task_button_white(void *pvParameters) {
+  int button_state = 0;
+  bool is_button_press = false;
+  pinMode(PIN_BUTTON_WHITE, INPUT);
+  
+  for(;;) {
+    button_state = digitalRead(PIN_BUTTON_WHITE);
+    
+    if(button_state == HIGH && !is_button_press) {
+      is_button_press = true;
+
+      if(display_interface == 0) {
+        if(display_opcao_selecionada < display_limite_opcao) {
+          display_opcao_selecionada++;
+        } else {
+          display_opcao_selecionada = 1;
+        }
+        
+      } else if(display_interface == 1) {
+        if(display_opcao_selecionada > 0) {
+          display_opcao_selecionada--;
+        } else {
+          display_interface = 0;
+          display_opcao_selecionada = 0;
+        }
+
+      } else if (display_interface == 3) {
+        display_interface = 0;
+        display_opcao_selecionada = 0;
+      }
+
+    } else if(button_state == LOW && is_button_press) {
+      is_button_press = false;
+    }
+
+    vTaskDelay(500 / portTICK_PERIOD_MS);
+  }
+}
---
+++ audiometro/player.h
@@ -0,0 +1,108 @@
+#include "DFRobotDFPlayerMini.h"
+#include "SoftwareSerial.h"
+
+#ifndef PIN_MP3_TX
+#define PIN_MP3_TX 1
+#endif
+
+#ifndef PIN_MP3_RX
+#define PIN_MP3_RX 3
+#endif
+
+#ifndef BUSY_PIN
+#define BUSY_PIN 5
+#endif
+
+// #ifndef CONFIRM_PIN
+// #define CONFIRM_PIN 18
+// #endif
+
+// #ifndef SELECT_PIN
+// #define SELECT_PIN 19
+// #endif
+
+DFRobotDFPlayerMini df;
+TaskHandle_t handlePlayFrequencies;
+SoftwareSerial softwareSerial(PIN_MP3_RX, PIN_MP3_TX);
+
+bool perdaAuditiva;
+bool isButtonPressed;
+
+// void taskButtonState(void* pvParameters) {
+//   int buttonState;
+//   bool is_button_press = false;
+//   isButtonPressed = false;
+
+//   pinMode(CONFIRM_PIN, INPUT);
+
+//   for(;;) {
+//     buttonState = digitalRead(CONFIRM_PIN);
+
+//     // Verifica se o botao foi apertado
+//     if(buttonState == HIGH && !is_button_press) {
+//       isButtonPressed = true;
+//     }
+//   }
+// }
+
+void taskPlayFrequencies(void* pvParameters) {
+  uint8_t currentTrack = 1;
+  perdaAuditiva = false;
+  int time_flag1;
+  int time_flag2;
+  int time_now;
+
+  // pinMode(SELECT_PIN, INPUT);
+  pinMode(BUSY_PIN, INPUT);
+  
+  if(df.begin(softwareSerial)) {  
+    Serial.println("Connected!");
+    df.volume(0);
+    df.play(1);
+    Serial.print("Playing Track: ");
+    Serial.println(currentTrack);
+
+    time_flag1 = millis();
+    time_flag2 = millis();
+
+    for(;;) {
+      time_now = millis();
+
+      //TODO: ajustar tempo de duracao de cada som
+      if(time_now - time_flag1 > 30000 || isButtonPressed) {
+        if(df.readVolume() > 4 && !isButtonPressed) perdaAuditiva = true;
+
+        // Toca o proximo audio
+        df.volume(0);
+        currentTrack += 1;
+        isButtonPressed = false;
+
+        // Verifica se ha audio
+        if (currentTrack > 6) {
+          Serial.println("Finished Frequencies.");
+          if (perdaAuditiva) Serial.println("Unfortunately, you have some kind of hearing loss."); 
+          else Serial.println("You're hearing is all good!");
+          vTaskDelete(handlePlayFrequencies);
+        }
+
+        // Proxima frequencia
+        df.next();
+        time_flag1 = millis();
+        Serial.print("Playing Track: ");
+        Serial.println(currentTrack);
+      }
+
+      // Aumenta o volume
+      if(time_now - time_flag2 > 1000) {
+        if (!isButtonPressed) df.volumeUp();
+        Serial.println(df.readVolume());
+        time_flag2 = millis();
+      }
+
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+  }
+  else {
+    Serial.println("Connection to DFPlayer Mini failed!");
+  }
+}
